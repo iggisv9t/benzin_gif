308e9a3543bf7e9491ef093cc84072260d7e8346
Sviatoslav Kovalev <iggisv9t@gmail.com>
2019-02-07 17:47:40 +0300

    add conversion to mp4

--- benzin_gif.py
+++ benzin_gif.py
@@ -26,3 +26,4 @@ if __name__ == "__main__":
 
     generate_frames(path)
     os.system('convert pics/*.png -delay 0.001 pics/output.gif')
+    os.system('ffmpeg -i pics/output.gif -movflags faststart -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" pics/video.mp4')
